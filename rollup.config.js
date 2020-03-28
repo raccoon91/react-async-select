3f4e6cff78fda06830dafa01dd530f58211241ff
raccoon91 <qsx314@gmail.com>
2020-03-28 20:49:17 +0900

    add rollup for npm package build

---
+++ rollup.config.js
@@ -0,0 +1,44 @@
+import typescript from "rollup-plugin-typescript2";
+import commonjs from "rollup-plugin-commonjs";
+import external from "rollup-plugin-peer-deps-external";
+import url from "rollup-plugin-url";
+import svgr from "@svgr/rollup";
+import resolve from "rollup-plugin-node-resolve";
+
+import pkg from "./package.json";
+
+export default {
+  input: "src/components/index.tsx",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+      exports: "named",
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
+      format: "es",
+      exports: "named",
+      sourcemap: true,
+    },
+  ],
+  external: ["styled-components"],
+  plugins: [
+    external(),
+    resolve(),
+    url(),
+    svgr(),
+    typescript({
+      rollupCommonJSResolveHack: true,
+      clean: true,
+    }),
+    commonjs({
+      include: ["node_modules/**"],
+      namedExports: {
+        "node_modules/react/react.js": ["Children", "Component", "PropTypes", "createElement"],
+        "node_modules/react-dom/index.js": ["render"],
+      },
+    }),
+  ],
+};
