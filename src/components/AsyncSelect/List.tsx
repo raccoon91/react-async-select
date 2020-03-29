e065c80126f83a166db3d1879f13338fa3be5f16
raccoon91 <qsx314@gmail.com>
2020-03-29 17:35:11 +0900

    add AsyncSelect component render search data & add event handler

---
+++ src/components/AsyncSelect/List.tsx
@@ -0,0 +1,65 @@
+import React, { FC } from "react";
+import styled from "styled-components";
+
+import { ListItem } from "./index";
+
+interface IListContainerProps {
+  height?: string;
+}
+const ListContainer = styled.div<IListContainerProps>`
+  overflow-y: auto;
+  position: absolute;
+  top: ${({ height }): string => height || "2rem"};
+  width: 100%;
+  max-height: 25rem;
+  border: 1px solid black;
+  background-color: white;
+  box-sizing: border-box;
+  z-index: 2;
+`;
+
+const Item = styled.div`
+  padding: 0.3rem 0.2rem;
+
+  &:hover {
+    background-color: blue;
+    color: white;
+  }
+`;
+const EmptyData = styled.div`
+  padding: 1rem 0;
+  color: gray;
+  text-align: center;
+`;
+
+interface IListProps {
+  height?: string;
+  message?: string;
+  onClickItem: (data: ListItem) => void;
+  handleBlur: () => void;
+  debouncedList: ListItem[];
+}
+const List: FC<IListProps> = ({ height, message, onClickItem, handleBlur, debouncedList }) => {
+  const handleSelectItem = (data: ListItem) => {
+    return (): void => {
+      if (onClickItem) onClickItem(data);
+      handleBlur();
+    };
+  };
+
+  return (
+    <ListContainer height={height}>
+      {debouncedList.length ? (
+        debouncedList.map((listItem, index) => (
+          <Item key={index} onClick={handleSelectItem(listItem)}>
+            {listItem.name}
+          </Item>
+        ))
+      ) : (
+        <EmptyData>{message || "empty"}</EmptyData>
+      )}
+    </ListContainer>
+  );
+};
+
+export default List;
---
+++ src/components/AsyncSelect/index.tsx
@@ -0,0 +1,169 @@
+import React, { FC, useState } from "react";
+import styled, { css, FlattenSimpleInterpolation } from "styled-components";
+
+import Maybe from "../../helper/Maybe";
+import List from "./List";
+
+import { ReactComponent as Icon } from "../../images/angle-down.svg";
+
+const AsyncContainer = styled.div`
+  position: relative;
+  background-color: white;
+  z-index: 2;
+`;
+
+const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: black;
+  z-index: 1;
+`;
+
+interface IAsyncWrapperProps {
+  width?: string;
+  height?: string;
+}
+const AsyncWrapper = styled.div<IAsyncWrapperProps>`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: ${({ width }): string => width || "10rem"};
+  height: ${({ height }): string => height || "2rem"};
+  padding: 0 0.5rem;
+  border: 1px solid black;
+  box-sizing: border-box;
+`;
+
+const AsyncInputWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 90%;
+`;
+
+const DisplayWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
+const Displayedvalue = styled.div`
+  width: 100%;
+  background-color: white;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow-x: hidden;
+`;
+
+const AsyncInput = styled.input`
+  width: 100%;
+  height: 100%;
+  border: none;
+  outline: none;
+`;
+
+interface IAngleIconProps {
+  open: boolean;
+}
+const AngleIcon = styled(Icon)<IAngleIconProps>`
+  width: 1rem;
+  height: 1rem;
+  fill: grey;
+  transition: transform 0.3s ease-out;
+  cursor: pointer;
+
+  ${({ open }): FlattenSimpleInterpolation =>
+    open
+      ? css`
+          transform: rotate(180deg);
+        `
+      : css`
+          transform: rotate(0deg);
+        `}
+`;
+
+export interface ListItem {
+  name: string;
+  value: string;
+}
+
+interface IAsyncProps {
+  width?: string;
+  height?: string;
+  inputValue: string;
+  displayedValue?: string;
+  handleChangeInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onClickItem: (data: ListItem) => void;
+  debouncedList: ListItem[];
+  message?: string;
+}
+
+const Async: FC<IAsyncProps> = ({
+  width,
+  height,
+  inputValue,
+  displayedValue,
+  handleChangeInput,
+  onClickItem,
+  debouncedList,
+  message,
+}) => {
+  const [isOpenList, setIsOpenList] = useState<boolean>(false);
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+
+  const handleClick = (): void => {
+    if (isOpenList) {
+      setIsOpenList(false);
+    } else {
+      setIsOpenList(true);
+    }
+  };
+
+  const handleFocus = (): void => {
+    setIsFocused(true);
+    setIsOpenList(true);
+  };
+
+  const handleBlur = (): void => {
+    setIsFocused(false);
+    setIsOpenList(false);
+  };
+
+  return (
+    <>
+      <Maybe isOpen={isOpenList}>
+        <Overlay onClick={handleBlur} />
+      </Maybe>
+      <AsyncContainer>
+        <AsyncWrapper width={width} height={height}>
+          <AsyncInputWrapper>
+            {displayedValue && !isFocused ? (
+              <DisplayWrapper onClick={handleFocus}>
+                <Displayedvalue>{displayedValue}</Displayedvalue>
+              </DisplayWrapper>
+            ) : null}
+            <AsyncInput value={inputValue} onChange={handleChangeInput} onFocus={handleFocus} />
+          </AsyncInputWrapper>
+          <AngleIcon open={isOpenList} onClick={handleClick} />
+        </AsyncWrapper>
+        <Maybe isOpen={isOpenList}>
+          <List
+            height={height}
+            message={message}
+            onClickItem={onClickItem}
+            handleBlur={handleBlur}
+            debouncedList={debouncedList}
+          />
+        </Maybe>
+      </AsyncContainer>
+    </>
+  );
+};
+
+export default Async;
