47ba7026b22ac5bf090d12987e0923642963cdc3
raccoon91 <qsx314@gmail.com>
2020-04-03 22:17:09 +0900

    add default style & customStyle props

--- src/components/AsyncSelect/List.tsx
+++ src/components/AsyncSelect/List.tsx
@@ -1,47 +1,50 @@
 import React, { FC } from "react";
-import styled from "styled-components";
+import styled, { css, FlattenSimpleInterpolation } from "styled-components";
 
-import { ListItem } from "./index";
+import { ListItem, StyleObject } from "./index";
 
 interface IListContainerProps {
-  height?: string;
+  listContainerStyle: StyleObject;
 }
 const ListContainer = styled.div<IListContainerProps>`
-  overflow-y: auto;
-  position: absolute;
-  top: ${({ height }): string => height || "2rem"};
-  width: 100%;
-  max-height: 25rem;
-  margin-top: 0.5rem;
-  border: 1px solid black;
-  background-color: white;
-  box-sizing: border-box;
-  z-index: 2;
+  ${({ listContainerStyle }): FlattenSimpleInterpolation =>
+    css`
+      ${listContainerStyle}
+    `}
 `;
 
-const Item = styled.div`
-  padding: 0.3rem 0.2rem;
-
-  &:hover {
-    background-color: blue;
-    color: white;
-    cursor: pointer;
-  }
+interface IItemProps {
+  listItemStyle: StyleObject;
+}
+const Item = styled.div<IItemProps>`
+  ${({ listItemStyle }): FlattenSimpleInterpolation =>
+    css`
+      ${listItemStyle}
+    `}
 `;
+
 const EmptyData = styled.div`
-  padding: 1rem 0;
+  padding: 0.5rem 0;
   color: gray;
   text-align: center;
 `;
 
 interface IListProps {
-  height?: string;
   message?: string;
   onClickItem: (data: ListItem) => void;
   handleBlur: () => void;
   debouncedList: ListItem[];
+  listContainerStyle: StyleObject;
+  listItemStyle: StyleObject;
 }
-const List: FC<IListProps> = ({ height, message, onClickItem, handleBlur, debouncedList }) => {
+const List: FC<IListProps> = ({
+  message,
+  onClickItem,
+  handleBlur,
+  debouncedList,
+  listContainerStyle,
+  listItemStyle,
+}) => {
   const handleSelectItem = (data: ListItem) => {
     return (): void => {
       if (onClickItem) onClickItem(data);
@@ -50,10 +53,10 @@ const List: FC<IListProps> = ({ height, message, onClickItem, handleBlur, deboun
   };
 
   return (
-    <ListContainer height={height}>
+    <ListContainer listContainerStyle={listContainerStyle}>
       {debouncedList.length ? (
         debouncedList.map((listItem, index) => (
-          <Item key={index} onClick={handleSelectItem(listItem)}>
+          <Item key={index} onClick={handleSelectItem(listItem)} listItemStyle={listItemStyle}>
             {listItem.name}
           </Item>
         ))
--- src/components/AsyncSelect/index.tsx
+++ src/components/AsyncSelect/index.tsx
@@ -1,10 +1,49 @@
 import React, { FC, useState, useRef } from "react";
-import styled, { css, FlattenSimpleInterpolation } from "styled-components";
+import styled, { css, FlattenSimpleInterpolation, CSSObject } from "styled-components";
 
 import Maybe from "../../helper/Maybe";
 import List from "./List";
 
 import { ReactComponent as Icon } from "../../images/angle-down.svg";
+
+export type StyleObject = CSSObject;
+
+const controlerDefaultStyle: StyleObject = {
+  width: "10rem",
+  height: "2rem",
+  padding: "0 0.5rem",
+  border: "1px solid black",
+};
+
+const inputDefaultStyle: StyleObject = {
+  width: "100%",
+  height: "100%",
+  border: "none",
+  outline: "none",
+  "box-sizing": "border-box",
+};
+
+const listContainerDefaultStyle: StyleObject = {
+  "overflow-y": "auto",
+  position: "absolute",
+  top: "2rem",
+  width: "100%",
+  "max-height": "25rem",
+  "margin-top": "0.5rem",
+  border: "1px solid black",
+  "background-color": "white",
+  "box-sizing": "border-box",
+  "z-index": "2",
+};
+
+const listItemDefaultStyle: StyleObject = {
+  padding: "0.3rem 0.2rem",
+  "&:hover": {
+    "background-color": "blue",
+    color: "white",
+    cursor: "pointer",
+  },
+};
 
 const AsyncContainer = styled.div`
   position: relative;
@@ -22,24 +61,24 @@ const Overlay = styled.div`
 `;
 
 interface IAsyncWrapperProps {
-  width?: string;
-  height?: string;
+  controlerStyle: StyleObject;
 }
 const AsyncWrapper = styled.div<IAsyncWrapperProps>`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  width: ${({ width }): string => width || "10rem"};
-  height: ${({ height }): string => height || "2rem"};
-  padding: 0 0.5rem;
-  border: 1px solid black;
   box-sizing: border-box;
+
+  ${({ controlerStyle }): FlattenSimpleInterpolation =>
+    css`
+      ${controlerStyle}
+    `};
 `;
 
 const AsyncInputWrapper = styled.div`
   position: relative;
   width: 100%;
-  height: 90%;
+  height: 100%;
 `;
 
 const DisplayWrapper = styled.div`
@@ -60,11 +99,14 @@ const Displayedvalue = styled.div`
   overflow-x: hidden;
 `;
 
-const AsyncInput = styled.input`
-  width: 100%;
-  height: 100%;
-  border: none;
-  outline: none;
+interface IAsyncInputProps {
+  inputStyle: StyleObject;
+}
+const AsyncInput = styled.input<IAsyncInputProps>`
+  ${({ inputStyle }): FlattenSimpleInterpolation =>
+    css`
+      ${inputStyle}
+    `}
 `;
 
 interface IAngleIconProps {
@@ -92,30 +134,40 @@ export interface ListItem {
   value: string;
 }
 
-interface IAsyncProps {
-  width?: string;
-  height?: string;
+export interface IAsyncProps {
   inputValue: string;
   displayedValue?: string;
   handleChangeInput: (value: string) => void;
   onClickItem: (data: ListItem) => void;
   debouncedList: ListItem[];
   message?: string;
+  style?: {
+    controlerStyle?: (props: StyleObject) => StyleObject;
+    inputStyle?: (props: StyleObject) => StyleObject;
+    listContainerStyle?: (props: StyleObject) => StyleObject;
+    listItemStyle?: (props: StyleObject) => StyleObject;
+  };
 }
 
 const Async: FC<IAsyncProps> = ({
-  width,
-  height,
   inputValue,
   displayedValue,
   handleChangeInput,
   onClickItem,
   debouncedList,
   message,
+  style,
 }) => {
   const inputRef = useRef<HTMLInputElement>(null);
   const [isOpenList, setIsOpenList] = useState<boolean>(false);
   const [isFocused, setIsFocused] = useState<boolean>(false);
+
+  const controlerStyle =
+    style && style.controlerStyle ? style.controlerStyle(controlerDefaultStyle) : controlerDefaultStyle;
+  const inputStyle = style && style.inputStyle ? style.inputStyle(inputDefaultStyle) : inputDefaultStyle;
+  const listContainerStyle =
+    style && style.listContainerStyle ? style.listContainerStyle(listContainerDefaultStyle) : listContainerDefaultStyle;
+  const listItemStyle = style && style.listItemStyle ? style.listItemStyle(listItemDefaultStyle) : listItemDefaultStyle;
 
   const handleFocus = (): void => {
     if (inputRef.current) {
@@ -152,7 +204,7 @@ const Async: FC<IAsyncProps> = ({
         <Overlay onClick={handleBlur} />
       </Maybe>
       <AsyncContainer>
-        <AsyncWrapper width={width} height={height}>
+        <AsyncWrapper controlerStyle={controlerStyle}>
           <AsyncInputWrapper>
             {displayedValue && !isFocused ? (
               <DisplayWrapper onClick={handleFocus}>
@@ -162,20 +214,22 @@ const Async: FC<IAsyncProps> = ({
             <AsyncInput
               ref={inputRef}
               value={inputValue}
-              placeholder="placeholder"
+              placeholder="검색어를 입력해주세요."
               onChange={handleChange}
               onFocus={handleFocus}
+              inputStyle={inputStyle}
             />
           </AsyncInputWrapper>
           <AngleIcon open={isOpenList} onClick={handleClick} />
         </AsyncWrapper>
         <Maybe isOpen={isOpenList}>
           <List
-            height={height}
             message={message}
             onClickItem={onClickItem}
             handleBlur={handleBlur}
             debouncedList={debouncedList}
+            listContainerStyle={listContainerStyle}
+            listItemStyle={listItemStyle}
           />
         </Maybe>
       </AsyncContainer>
